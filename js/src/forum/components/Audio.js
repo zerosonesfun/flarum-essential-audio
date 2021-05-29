dbc7565a7028deee51520a71235cbcd2f96e8a49
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2021-05-29 08:16:45 -0400

    Update Audio.js

--- js/src/forum/components/Audio.js
+++ js/src/forum/components/Audio.js
@@ -9,7 +9,7 @@ export default class Audio extends Component {
             body.classList.add("Audio");
             extend(PostStream.prototype, 'view', function(vnode) {
                 if (vnode.children && vnode.children.splice) {
-                    const insert = <script src="/assets/extensions/zerosonesfun-essential-audio/essential-audio.js"></script>;
+                    const insert = <script src="../assets/extensions/zerosonesfun-essential-audio/essential-audio.js"></script>;
                     vnode.children.splice(1, 0, insert);
                   }
               });
